672f61d396aaf8577e93c9026f8fd41fbe36b0f1
admin <admin@DESKTOP-RMKFLBH>
2023-10-25 11:16:44 +0900

    커밋

--- helloWeb/src/main/webapp/js/function5.js
+++ helloWeb/src/main/webapp/js/function5.js
@@ -1,7 +1,18 @@
 // function5.js
 
-function sum(a=0, b=0, c=0){ // parameters. (매개변수)
-	return a + b + c;
+let sum1 = 0;
+[10,20,30].forEach(function(num){
+	sum1 += num; // consumer : 매개값을 소진, 반환값은 없음.
+})
+
+sum = [10,20,30].reduce(function(acc, item, idx, ary){
+	console.log(acc, item)
+	return item;	
+})
+
+function sum(a=0, b=0, ...args){ // parameters. (매개변수)
+	console.log(args);
+	return a + b; //args.forEach;
 }
 
-console.log(sum(10,20,30,40,50)); // arguments. (매개값)
+console.log(10,20,30,40,50,60); // arguments. (매개값)
